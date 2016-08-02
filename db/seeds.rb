23291e8eebb4e58b39878a1e39737c8ca021b1fb
StlMaris123 <stellamarisnjage@gmail.com>
2016-08-02 11:59:21 +0300

    activating seed users

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,20 @@ User.create!(name: "Stella Njage",
               email: "ste@njage.com",
               password: "rainbow",
               password_confirmation: "rainbow",
-              admin: true)
+              admin: true,
+              activated: true,
+              activated_at: Time.zone.now)
+5.times do |n|
+  name = Faker::Name.name
+  email = "ste-#{n+1}@gmail.com"
+  password = "password"
+User.create!(name: name,
+             email: email,
+             password: password,
+             password_confirmation: password,
+             activated: true,
+             activated_at: Time.zone.now)
+end
 100.times do |d|
   title = Faker::Book.title
   link  = Faker::Internet.url
