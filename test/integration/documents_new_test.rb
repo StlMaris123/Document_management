2d6c36a5c7c0cdd78f00e65d4c3eb4af27f78945
StlMaris123 <stellamarisnjage@gmail.com>
2016-08-05 19:50:30 +0300

    test for successful document creation

--- test/integration/documents_new_test.rb
+++ test/integration/documents_new_test.rb
@@ -16,6 +16,21 @@ class DocumentsNewTest < ActionDispatch::IntegrationTest
     end
     assert_template 'documents/new'
   end
+  test "successful document creation" do
+    log_in_as(@user)
+    get new_document_path
+    assert_template 'documents/new'
+    assert_difference 'Document.count', 1 do
+      post documents_path, params: { document: { title: "Love Affairs",
+                                                 tag: "Love",
+                                                 link: "https://www.love.com",
+                                                 department: "Finance"
 
+      }
 
+      }
+    end
+    assert_redirected_to documents_url
+    assert_not flash.empty?
+  end
 end
