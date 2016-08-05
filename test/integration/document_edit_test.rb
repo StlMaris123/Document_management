3ba58184352d869a23245e15313439c51f00cf2d
StlMaris123 <stellamarisnjage@gmail.com>
2016-08-05 20:28:23 +0300

    test for successful edit

--- test/integration/document_edit_test.rb
+++ test/integration/document_edit_test.rb
@@ -10,12 +10,33 @@ class DocumentEditTest < ActionDispatch::IntegrationTest
     get edit_document_path(@document)
     assert_template 'documents/edit'
     patch document_path(@document), params: { document:{ title: "",
-                                               tag: "",
-                                               link: "zxcvbbnm",
-                                               department: ""
+                                                         tag: "",
+                                                         link: "zxcvbbnm",
+                                                         department: ""
 
-     }
+    }
     }
     assert_template 'documents/edit'
+  end
+
+  test "successful edit" do
+    log_in_as(@user)
+    get edit_document_path(@document)
+    assert_template 'documents/edit'
+    title      = "A Scanner Darkly"
+    tag        = "Speech"
+    link       = "http://bogisichdurgan.io/israel"
+    department = "Credit"
+    patch document_path(@document),params: { document: { title: name,
+                                                         tag: tag,
+                                                         link: link, 
+                                                         department: department} }
+    assert_not flash.empty?
+    assert_redirected_to documents_url
+    @document.reload
+    assert_equal @document.title, title
+    assert_equal @document.tag, tag
+    assert_equal @document.link, link
+    assert_equal @document.department, department
   end
 end
