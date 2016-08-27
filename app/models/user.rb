a19ff667de268dcfbc873692eaacb9ed132ae71a
StlMaris123 <stellamarisnjage@gmail.com>
2016-08-27 17:24:57 +0300

    add the remember method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -12,5 +13,15 @@ class User < ApplicationRecord
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
-  
+
+  #Returns a random token
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  #Rememebers a user in the database for use in persistent sessions
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
 end
