9202848260321909927468a80220545f0aca043e
StlMaris123 <stellamarisnjage@gmail.com>
2016-08-27 11:47:19 +0300

    user show view with name and gravatar

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,6 @@ Rails.application.routes.draw do
 
   get 'contact' => 'fixed_pages#contact'
 
-  get 'signup' => 'users#new'
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
