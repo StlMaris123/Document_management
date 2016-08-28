2da19801b57bdd7dba50bd785a066e78ea61ffcc
StlMaris123 <stellamarisnjage@gmail.com>
2016-08-28 20:11:01 +0300

    test for successful edit

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -14,4 +14,19 @@ class UsersEditTest < ActionDispatch::IntegrationTest
                                               password_confirmation: "bow"}}
     assert_template 'users/edit'
   end
+  test "successful edit" do
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    name = "Stella Njage"
+    email = "ste@gmmail.com"
+    patch user_path(@user), params:{ user: {  name: name,
+                                              email: email,
+                                              password:    "",
+                                              password_confirmation: ""} }
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal @user.name, name
+    assert_equal @user.email, email
+  end
 end
