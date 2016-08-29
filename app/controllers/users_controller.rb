8fc98d6494ccaf382b80e4b5b887c655c72ef48f
StlMaris123 <stellamarisnjage@gmail.com>
2016-08-29 07:27:01 +0300

    display all the users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
- before_action :logged_in_user, only: [:edit, :update]
+ before_action :logged_in_user, only: [:index, :edit, :update]
  before_action :correct_user, only: [:edit, :update]
   def new
     @user = User.new
@@ -18,6 +18,10 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+  end
+
+  def index
+    @users = User.all
   end
 
   def edit
@@ -42,6 +46,7 @@ class UsersController < ApplicationController
   # confirms a logged-in user
   def logged_in_user
     unless logged_in?
+      store_location
       flash[:alert] = "Please log in first" 
       redirect_to login_url
     end
